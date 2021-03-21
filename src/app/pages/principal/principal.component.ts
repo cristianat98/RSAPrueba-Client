f9fff69e078eb7f18e44405295fcdb769bb3fe99
cristianat98 <cristianarmestotejedo@gmail.com>
2021-03-21 13:29:43 +0100

    Cliente acabado!

--- src/app/pages/principal/principal.component.ts
+++ src/app/pages/principal/principal.component.ts
@@ -11,14 +11,20 @@ export class PrincipalComponent implements OnInit {
   constructor(private pruebaService: PruebaService) { }
 
   mensaje: String;
-
+  usuario: String;
+  mensajes: any[] = [];
   ngOnInit(): void {
   }
 
   enviar(): void{
-    this.pruebaService.getMensaje(this.mensaje).subscribe(data => {
-      console.log("ENVIADO!!");
-      console.log("DATA", data);
+    const mensaje = {
+      usuario: this.usuario,
+      mensaje: this.mensaje
+    }
+    this.pruebaService.getMensaje(mensaje).subscribe(data => {
+      this.mensajes.push(data);
+    }, error => {
+      console.log("ERROR!!")
     })
   }
 }
