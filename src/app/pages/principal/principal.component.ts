d19313cb16bfce5995c6248823b27b8540b30a3a
cristianat98 <cristianarmestotejedo@gmail.com>
2021-04-08 17:41:37 +0200

    falta descifrar cn AES

--- src/app/pages/principal/principal.component.ts
+++ src/app/pages/principal/principal.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PruebaService } from 'src/app/services/prueba.service';
+import * as bigintConversion from 'bigint-conversion';
 
 @Component({
   selector: 'app-principal',
@@ -10,21 +11,28 @@ export class PrincipalComponent implements OnInit {
 
   constructor(private pruebaService: PruebaService) { }
 
-  mensaje: String;
-  usuario: String;
+  mensaje: string;
+  usuario: string;
   mensajes: any[] = [];
+
   ngOnInit(): void {
+    this.pruebaService.getClaves();
   }
 
   enviar(): void{
     const mensaje = {
       usuario: this.usuario,
-      mensaje: this.mensaje
+      mensaje: bigintConversion.textToBigint(this.mensaje)
     }
-    this.pruebaService.getMensaje(mensaje).subscribe(data => {
-      this.mensajes.push(data);
-    }, error => {
-      console.log("ERROR!!")
+    this.mensajes.push({
+      "usuario": "Server",
+      "mensaje": "Enviando..."
     })
+    this.pruebaService.getMensaje(mensaje);
+    /*this.pruebaService.getMensaje(mensaje).subscribe(data => {
+      this.mensajes[this.mensajes.length - 1] = data;
+    }, () => {
+      this.mensajes[this.mensajes.length - 1].mensaje = "NO SE HA PODIDO ENVIAR";
+    })*/
   }
 }
--- src/app/services/prueba.service.ts
+++ src/app/services/prueba.service.ts
@@ -2,15 +2,47 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
+import * as bigintConversion from 'bigint-conversion'
+import * as bcu from 'bigint-crypto-utils'
 
 @Injectable({
   providedIn: 'root'
 })
 export class PruebaService {
 
+  keyAES;
+  keyRSAe: bigint;
+  keyRSAn: bigint;
   constructor(private http: HttpClient) { }
 
-  getMensaje(mensaje: any): Observable<any>{
-    return this.http.post<any>(environment.apiURL + "/mensaje", mensaje);
+  getMensaje(mensaje: any){
+    mensaje.mensaje = bcu.modPow(mensaje.mensaje, this.keyRSAe, this.keyRSAn)
+    mensaje.mensaje = bigintConversion.bigintToHex(mensaje.mensaje)
+    this.http.post<any>(environment.apiURL + "/mensaje", mensaje).subscribe(async data => {
+      /*return */ const mensaje = await crypto.subtle.decrypt(
+        {
+          name: "AES-GCM",
+          iv: new Uint8Array(data.iv)
+        },
+        this.keyAES,
+        bigintConversion.hexToBuf(data.mensaje)
+      )
+    });
+  }
+
+  async getClaves() {
+    const keyAESHex = "95442fa551e13eacedea3e79f0ec1e63513cc14a9dbc4939ad70ceb714b44b8f";
+    this.keyAES = new Uint8Array(bigintConversion.hexToBuf(keyAESHex));
+    this.keyAES = await crypto.subtle.importKey(
+      "raw",
+      this.keyAES,
+      "AES-GCM",
+      true,
+      ["encrypt", "decrypt"]
+    )
+    this.http.get<any>(environment.apiURL + "/rsa").subscribe(data => {
+      this.keyRSAe = bigintConversion.hexToBigint(data.publicKey.e);
+      this.keyRSAn = bigintConversion.hexToBigint(data.publicKey.n);
+    });
   }
 }
